8a5aaaf0ada7443125bf075208437516330dc40b
zalexander.roca <zalexander.roca>
2026-07-15 12:05:26 +0800

    fix error import DriverFactory

--- shared/src/webMain/kotlin/ph/com/alexcr/tracker/data/database/DriverFactory.web.kt
+++ shared/src/webMain/kotlin/ph/com/alexcr/tracker/data/database/DriverFactory.web.kt
@@ -1,6 +1,5 @@
 package ph.com.alexcr.tracker.data.database
 
-import app.cash.sqldelight.async.coroutines.await
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.worker.createDefaultWebWorkerDriver
 import ph.com.alexcr.tracker.database.BudgetTrackerDatabase
