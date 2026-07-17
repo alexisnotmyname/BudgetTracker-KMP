aaf4bda8cb88c342c1c465c1c401b1f256fdcf73
zalexander.roca <zalexander.roca>
2026-07-17 09:41:45 +0800

    adding splash screen and icon

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,6 +17,8 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(libs.koin.android)
 
+    implementation(libs.core.splashscreen)
+
     implementation(libs.compose.uiToolingPreview)
     debugImplementation(libs.compose.uiTooling)
 }
--- androidApp/src/main/kotlin/ph/com/alexcr/MainActivity.kt
+++ androidApp/src/main/kotlin/ph/com/alexcr/MainActivity.kt
@@ -6,13 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import ph.com.alexcr.app.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
+        installSplashScreen()
         setContent {
             App()
         }
